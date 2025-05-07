dcff0e34eb15a36980fb52d671699f8ebe55fb6f
Branko Beovic <tu_email@ejemplo.com>
2025-05-07 18:01:40 -0400

    Inicializa proyecto examen computación móvil

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/cartscreen.dart';
+import 'package:flutter_application_1/screen/screen.dart';
+
+class AppRoutes {
+  static const initialRoute = 'login';
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'login': (BuildContext context) => const LoginScreen(),
+    'list': (BuildContext context) => const ListProductScreen(),
+    'edit': (BuildContext context) => const EditProductScreen(),
+    'add_user': (BuildContext context) => const RegisterUserScreen(),
+    'cart': (BuildContext context) => CartScreen(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+      builder: (context) => const ErrorScreen(),
+    );
+  }
+}
